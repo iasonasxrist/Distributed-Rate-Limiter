297722ab504618415bf070fe4d3d5c857ba50ef2
iasonasxrist <iasonasxrist@gmail.com>
2025-11-29 13:32:10 +0200

    rule service initiate

---
+++ RulesService/RulesSyncWorker.cs
@@ -0,0 +1,9 @@
+namespace RulesService;
+
+public sealed class RulesSyncWorker : BackgroundService
+{
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        throw new NotImplementedException();
+    }
+}
